c4cf0abd545dfa474232c69050e3a65f831ad348
Craig Disselkoen <cdisselk@eng.ucsd.edu>
2022-01-31 16:47:28 -0800

    whoops wrap that in a LLVM_DEBUG

--- llvm/lib/Transforms/Utils/DMS.cpp
+++ llvm/lib/Transforms/Utils/DMS.cpp
@@ -185,7 +185,7 @@ public:
         llvm_unreachable("fixpoint failure");
       }
       iterationCount++;
-      dbgs() << "DMS: starting iteration " << iterationCount << " through function " << F.getName() << "\n";
+      LLVM_DEBUG(dbgs() << "DMS: starting iteration " << iterationCount << " through function " << F.getName() << "\n");
       res = doIteration(NULL, false);
     }
 
